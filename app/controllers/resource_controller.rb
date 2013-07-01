cba8eedfccc80ccd03f3c8a41a7743289718cc62
jpsilvashy <jpsilvashy@gmail.com>
2013-07-01 05:29:33 -0700

    tidy up, also added delete method for removing a link, however it needs the short code as the param which isn't finished

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -1,3 +1,4 @@
+# POST /
 post '/' do
   if params[:url] and not params[:url].empty?
     @shortcode = SecureRandom.hex(4)
@@ -5,11 +6,20 @@ post '/' do
   end
 end
 
+# DELETE /:shortcode
+delete '/:shortcode' do
+  if params[:shortcode] and not params[:shortcode].empty?
+    settings.redis.del params[:shortcode]
+  end
+end
+
+# GET on index
 get '/' do
   @randomkey = settings.redis.get settings.redis.randomkey
   redirect @randomkey || '/'
 end
 
+# GET for shortcodes
 get '/:shortcode' do
   @url = settings.redis.get params[:shortcode]
   redirect @url || '/'
---
+++ utils/seed.sh
@@ -0,0 +1,8 @@
+http -f post treatyoself.herokuapp.com url='http://blog.zap2it.com/frominsidethebox/parks-and-rec-treat-yoursel.jpg'
+http -f post treatyoself.herokuapp.com url='http://oi52.tinypic.com/afihkh.jpg'
+http -f post treatyoself.herokuapp.com url='http://24.media.tumblr.com/tumblr_m8mtpz78vu1rs7wu4o1_r2_500.jpg'
+http -f post treatyoself.herokuapp.com url='http://michiganjournal.org/wp-content/uploads/2012/11/parks-and-rec-treat-yo-self.gif'
+http -f post treatyoself.herokuapp.com url='http://sherryndaniel.files.wordpress.com/2012/02/treat-to-self-2.jpg'
+http -f post treatyoself.herokuapp.com url='http://media.tumblr.com/tumblr_m8u0ziwt5i1rqpx0x.gif'
+http -f post treatyoself.herokuapp.com url='http://www.newdressaday.com/wp-content/uploads/2011/11/photo7.jpg'
+http -f post treatyoself.herokuapp.com url='http://25.media.tumblr.com/tumblr_lt2w75rKZO1qazkdco1_500.gif'
